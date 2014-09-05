a1308d4d3c148c270f325eb43940a7e05b3da1e7
Randy Leighton & Diego Avalon <student@epicodus.com>
2014-09-05 15:46:24 -0700

    link has many votes, user has many votes through links

--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -2,4 +2,6 @@ require 'rails_helper'
 
 describe Link do
 
+it { should have_many :votes}
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,5 +4,7 @@ describe User do
 
 it { should validate_uniqueness_of :email }
 
+it { should have_many :links }
+it { should have_many(:votes).through(:links)}
 
 end
