ff694ba199994bd74e57cc9b77723aa33322bbbf
Randy Leighton & Diego Avalon <student@epicodus.com>
2014-09-05 15:56:51 -0700

    link belongs to user

--- app/models/link.rb
+++ app/models/link.rb
@@ -2,4 +2,5 @@ class Link < ActiveRecord::Base
   has_many :votes
   has_many :comments
   has_many :points
+  belongs_to :user
 end
--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -5,5 +5,5 @@ describe Link do
 it { should have_many :votes }
 it { should have_many :comments }
 it { should have_many :points }
-
+it { should belong_to :user }
 end
