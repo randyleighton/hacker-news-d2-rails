c4fffa847bd1c2afec4c34d3f89ec1113a0fa255
Randy Leighton & Diego Avalon <student@epicodus.com>
2014-09-05 15:54:44 -0700

    link has many points

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,4 +1,5 @@
 class Link < ActiveRecord::Base
   has_many :votes
   has_many :comments
+  has_many :points
 end
--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 describe Link do
 
-it { should have_many :votes}
-it { should have_many :comments}
+it { should have_many :votes }
+it { should have_many :comments }
+it { should have_many :points }
 
 end
