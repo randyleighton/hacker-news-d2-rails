c0853b6aabc6337ad80a7df6c4131f9cb74e4e2f
Randy Leighton & Diego Avalon <student@epicodus.com>
2014-09-08 16:33:41 -0700

    display link vote count

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,2 +1,15 @@
 class VotesController < ApplicationController
+
+  def create
+    @current_link = Link.find(params[:link_id])
+    @current_link.vote.new(params[:vote])
+    if @vote.save
+      flash[:notice] = "Upvoted!"
+      redirect_to links_path
+    else
+      flash[:alert]= "Failed!"
+      redirect_to links_path
+    end
+  end
+
 end
