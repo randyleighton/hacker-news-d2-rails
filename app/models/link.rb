dd4001cde77231e7ec44fcb5d2c8afb210e69ab2
Randy Leighton & Diego Avalon <student@epicodus.com>
2014-09-08 09:28:43 -0700

    validate presence of url in Link

--- app/models/link.rb
+++ app/models/link.rb
@@ -6,5 +6,6 @@ class Link < ActiveRecord::Base
   belongs_to :user
 
   validates :name, presence: true
+  validates :url, presence: true
 
 end
--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -8,5 +8,6 @@ describe Link do
   it { should belong_to :user }
 
   it { should validate_presence_of :name }
+  it { should validate_presence_of :url }
 
 end
